d5fe76c163d0b1c826535faeb9850afc2d0450f1
malli1011 <mallikarjun.bandi@gmail.com>
2022-05-23 15:31:17 +0200

    deleting the banking project

--- BankingSystem/src/main/java/banking/ConsumerAccount.java
+++
@@ -1,8 +0,0 @@
-package banking;
-
-public class ConsumerAccount extends Account {
-    public ConsumerAccount(Person person, Long accountNumber, int pin, double currentBalance) {
-        // complete the function
-        super(person, accountNumber, pin, currentBalance);
-    }
-}
